73b09459de79c24916b8572a591ce1703b87a2e7
Rajat Agrawal <agrawalrajat357@gmail.com>
2024-07-31 12:28:46 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    
+    email: {type:String, required: true, unique: true},
     lastName: { type: String },
     address: { type: String },
     createdAt: { type: Date, default: Date.now },
